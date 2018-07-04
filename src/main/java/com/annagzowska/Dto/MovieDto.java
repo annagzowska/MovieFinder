71253182def6987a81552b6b78e39a0006108bbc
Anna Gzowska <a.gzowska@gmail.com>
2018-07-04 14:37:37 +0200

    adding database in progress

--- src/main/java/com/annagzowska/Dto/MovieDto.java
+++ src/main/java/com/annagzowska/Dto/MovieDto.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
@@ -20,9 +17,13 @@ public class MovieDto {
     @GeneratedValue
     private Long id;
 
+    @Column
     private String title;
 
+    @Column
     private int year;
 
+    @Column
+    @Lob
     private String description;
 }
